8c760ea35d1080a0b6294d276fb935f05b461366
Avishka Indula <avishkainduladev@gmail.com>
2022-05-24 08:26:21 +0530

    Installing Express with npm

---
+++ app.js
@@ -0,0 +1,4 @@
+// Before executing npm install command, we need to run npm init
+// After that, we need to install express by using "npm install express" command
+// When we install a package using npm, it also downloads all the other packages that the original package depends on.
+// So when we install expressJS package, it will also install more than 50 other packages with it.
